8757784af4700e992002b1c411b6ffffb445491d
danilchican <danilchican@mail.ru>
2017-02-06 19:46:53 +0300

    Added socialite package

--- www/config/app.php
+++ www/config/app.php
@@ -176,7 +176,11 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+		
+		/*
+		 * Own providers
+		 */
+		SocialiteProviders\Manager\ServiceProvider::class,
     ],
 
     /*
