75e84fa1a52f768a80801780eb8ae58a2dd772a7
danilchican <danilchican@mail.ru>
2017-02-27 00:05:08 +0300

    Fixed package & gulpfile

---
+++ www/gulpfile.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Vlad on 26.02.2017.
+ */
+const elixir = require('laravel-elixir');
+
+require('laravel-elixir-vue-2');
+
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for your application as well as publishing vendor resources.
+ |
+ */
+
+elixir((mix) => {
+    mix.sass('app.scss')
+    .webpack('app.js')
+    .webpack('account.js', 'public/js/account.js');
+});
