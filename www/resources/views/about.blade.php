62438b0f0d1763683fddc551e41524b1a652a55b
danilchican <danilchican@mail.ru>
2017-03-02 21:42:08 +0300

    Fixed team photos

--- www/resources/views/about.blade.php
+++ www/resources/views/about.blade.php
@@ -18,99 +18,38 @@
             background-color: inherit;
         }
 
-        .mate1 {
+        .team-info .mate {
             height: 175px;
             width: 175px;
             overflow: hidden;
             border-radius: 50%;
-
-            background-size: 200px 300px;
-            background-position-y: -35px;
-            background-position-x: center;
-
-            alignment: center;
-
-            margin: 0 auto;
-            top: 0;
-            left: 0;
-            right: 0;
-            bottom: 0;
+            position: relative;
+            display: inline-block;
         }
 
-        .mate2 {
-            height: 175px;
-            width: 175px;
-            overflow: hidden;
-            border-radius: 50%;
-
-            background-size: 540px 400px;
-            background-position-y: -85px;
-            background-position-x: -135px;
-
-            alignment: center;
-
-            margin: 0 auto;
-            top: 0;
-            left: 0;
-            right: 0;
-            bottom: 0;
+        .team-info .mate > .mate1 {
+            width: 178px;
+            margin-top: -13px;
         }
 
-        .mate3 {
-            height: auto;
-            width: 175px;
-            overflow: hidden;
-            border-radius: 50%;
-
-            background-size: 380px 250px;
-            background-position-y: -10px;
-            background-position-x: -135px;
-
-            alignment: center;
-
-            margin: 0 auto;
-            top: 0;
-            left: 0;
-            right: 0;
-            bottom: 0;
+        .team-info .mate > .mate2 {
+            width: 540px;
+            margin: -82px 0 0 -135px;
         }
 
-        .mate4 {
-            height: auto;
-            width: 175px;
-            overflow: hidden;
-            border-radius: 50%;
+        .team-info .mate > .mate3 {
+            width: 340px;
+            margin: -1px 0 0 -110px;
+        }
 
-            background-size: 680px 880px;
-            background-position-y: -115px;
-            background-position-x: center;
-
-            alignment: center;
-
-            margin: 0 auto;
-            top: 0;
-            left: 0;
-            right: 0;
-            bottom: 0;
+        .team-info .mate > .mate4 {
+            width: 695px;
+            margin: -115px 0 0 -255px;
         }
 
         .mate5 {
-            height: 175px;
-            width: 175px;
-            overflow: hidden;
-            border-radius: 50%;
-
-            background-size: 200px 200px;
-            background-position-y: center;
-            background-position-x: center;
-
-            alignment: center;
-
-            margin: 0 auto;
-            top: 0;
-            left: 0;
-            right: 0;
-            bottom: 0;
+            width: 180px;
+            margin: -1px 0 0 -2px;
         }
 
     </style>
@@ -125,31 +64,41 @@
         <h1>Our team</h1>
 
         <div class="style-first" style="padding-top: 10px; padding-bottom: 10px">
-            <img src="/image/team/alexandr.jpg" class="img-div mate1"/>
+            <div class="img-div mate">
+                <img src="/image/team/alexandr.jpg" class="mate1" />
+            </div>
             <h2>Alexandr Konikov</h2>
             <p class="lead" style="font-style: italic; font-size: 18px">Scrum Master</p>
         </div>
 
         <div class="style-second" style="background-color: white; padding-top: 50px; padding-bottom: 10px">
-            <img src="/image/team/ulyana.jpg" class="img-div mate2"/>
+            <div class="img-div mate">
+                <img src="/image/team/ulyana.jpg" class="mate2" />
+            </div>
             <h2>Ulyana Kiklevich</h2>
             <p class="lead" style="font-style: italic; font-size: 18px">Muse, Android Developer, QA</p>
         </div>
 
         <div class="style-third" style="padding-top: 50px; padding-bottom: 10px">
-            <img src="/image/team/vladislav.jpg" class="img-div mate3"/>
+            <div class="img-div mate">
+                <img src="/image/team/vladislav.jpg" class="mate3" />
+            </div>
             <h2>Vladislav Danilchick</h2>
             <p class="lead" style="font-style: italic; font-size: 18px">Back-end Developer, QA</p>
         </div>
 
         <div class="style-fourth" style="background-color: white; padding-top: 50px; padding-bottom: 10px">
-            <img src="/image/team/artyom.jpg" class="img-div mate4"/>
+            <div class="img-div mate">
+                <img src="/image/team/artyom.jpg" class="mate4" />
+            </div>
             <h2>Artyom Vasilevich</h2>
             <p class="lead" style="font-style: italic; font-size: 18px">Front-end Developer, QA</p>
         </div>
 
         <div class="style-fifth" style="padding-top: 50px; padding-bottom: 10px">
-            <img src="/image/team/andrey.jpg" class="img-div mate5"/>
+            <div class="img-div mate">
+                <img src="/image/team/andrey.png" class="mate5" />
+            </div>
             <h2>Andrey Gromyko</h2>
             <p class="lead" style="font-style: italic; font-size: 18px">Design, QA</p>
         </div>
