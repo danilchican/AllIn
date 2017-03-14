befa3215f73404752551c20e660f18b42f3a6272
Artem Vasilevich <joneshpx@gmail.com>
2017-03-14 21:37:23 +0300

    Fixed user profile avatar.

--- www/resources/views/account/index.blade.php
+++ www/resources/views/account/index.blade.php
@@ -13,8 +13,11 @@
         .user-info {
             background-color: white;
             border-radius: 5px;
+            border: 1px solid #d3e0e9;
             alignment: center;
             text-align: center;
+            padding: 20px 0;
+            margin-bottom: 10px;
         }
     </style>
 
@@ -54,7 +57,9 @@
                     <h4 style="margin-top:0;">{{ $user->name }}</h4>
 
                     @if($user->avatar)
-                        <img src="{{ $user->avatar }}" alt="User photo" style="max-width: 250px; " />
+                        <img src="{{ $user->avatar }}" alt="User photo" style="max-width: 200px;" />
+                    @else
+                        <img src="/image/no_avatar.jpg" alt="User photo" style="max-width: 200px;" />
                     @endif
                 </div>
 
