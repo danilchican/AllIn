e37913f8414f3662173ad0ccfe461831e8fe53b1
danilchican <danilchican@mail.ru>
2017-03-19 13:51:45 +0300

    Modified SocialContract

--- www/app/Helpers/SocialContract.php
+++ www/app/Helpers/SocialContract.php
@@ -5,4 +5,6 @@ namespace App\Helpers;
 interface SocialContract
 {
     public function getUserAvatarByProvider($providerName, $providerUser);
+    public function getVkontakteAvatar($providerUser);
+    public function getFacebookAvatar($providerUser);
 }
