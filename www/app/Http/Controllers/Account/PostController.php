669063756ab0b344fd9a72cc8c50f49956374e1d
danilchican <danilchican@mail.ru>
2017-03-20 15:54:25 +0300

    Created base method to store post

--- www/app/Http/Controllers/Account/PostController.php
+++ www/app/Http/Controllers/Account/PostController.php
@@ -11,7 +11,6 @@ class PostController extends Controller
     /**
      * Create a new controller instance.
      *
-     * @return void
      */
     public function __construct()
     {
@@ -20,6 +19,6 @@ class PostController extends Controller
 
     public function store(Request $request)
     {
-        //..
+        return dd($request->all());
     }
 }
