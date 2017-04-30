5a2487419b5c2da83f54dda3d2b0c498f1bfacab
avasilevich <joneshpx@gmail.com>
2017-04-30 22:37:51 +0300

    Modified statistic chart type.

--- www/resources/assets/js/account/components/Statistic.vue
+++ www/resources/assets/js/account/components/Statistic.vue
@@ -30,41 +30,38 @@
             var ctx = document.getElementById("myChart").getContext('2d');
 
             var myChart = new Chart(ctx, {
-                type: 'bar',
+                type: 'line',
                 data: {
                     labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
-                    datasets: [{
-                        label: ' Number of Posts',
-                        data: [12, 19, 13, 5, 7, 10, 21],
-                        backgroundColor: [
-                            'rgba(255, 99,  132, 0.5)',
-                            'rgba(54,  162, 235, 0.5)',
-                            'rgba(255, 206, 86,  0.5)',
-                            'rgba(75,  192, 192, 0.5)',
-                            'rgba(153, 102, 255, 0.5)',
-                            'rgba(255, 159, 64,  0.5)',
-                            'rgba(215, 136, 255, 0.5)'
-
-                        ],
-                        borderColor: [
-                            'rgba(255, 99,  132, 1)',
-                            'rgba(54,  162, 235, 1)',
-                            'rgba(255, 206, 86,  1)',
-                            'rgba(75,  192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64,  1)',
-                            'rgba(215, 136, 255, 1)'
-                        ],
-                        borderWidth: 3
-                    }]
+                    datasets: [
+                        {
+                            label: "My First dataset",
+                            fill: false,
+                            lineTension: 0.1,
+                            backgroundColor: "rgba(75,192,192,0.4)",
+                            borderColor: "rgba(75,192,192,1)",
+                            borderCapStyle: 'butt',
+                            borderDash: [],
+                            borderDashOffset: 0.0,
+                            borderJoinStyle: 'miter',
+                            pointBorderColor: "rgba(75,192,192,1)",
+                            pointBackgroundColor: "#fff",
+                            pointBorderWidth: 1,
+                            pointHoverRadius: 5,
+                            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                            pointHoverBorderColor: "rgba(220,220,220,1)",
+                            pointHoverBorderWidth: 2,
+                            pointRadius: 1,
+                            pointHitRadius: 10,
+                            data: [65, 59, 80, 81, 56, 55, 40],
+                            spanGaps: false,
+                        }
+                    ]
                 },
                 options: {
-                    responsive: false,
                     scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero:true
-                            }
+                        xAxes: [{
+                            display: true
                         }]
                     }
                 }
